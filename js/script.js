fee1ae5c8fa6dd617655586b2f5ea780f514522e
msidiqh991 <msidiqh991@gmail.com>
2023-10-02 22:47:47 +0700

    stuck in postbook()

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ modalButton.onclick = function(){
 
 closeButton.onclick = function() {
     getModal.style.display = "none";
-  }
+}
 
 window.onclick = function(event) {
     if (event.target == getModal) {
@@ -17,3 +17,49 @@ window.onclick = function(event) {
 }
 
 
+const books = [];
+const RENDER_EVENT = 'render-books';
+
+const SAVED_EVENT = 'saved-books';
+const STORAGE_KEY = 'BOOKS'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const submitForm = document.getElementById('form');
+
+    submitForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        addBook();
+    })
+
+    function addBook(){
+        const bookTitle = document.getElementById('title').value;
+        const bookAuthor = document.getElementById('author').value;
+        const bookYear = document.getElementById('year').value;
+
+        const generatedID = generatedId();
+        const bookObject = generatedBookObject(generatedID, bookTitle, bookAuthor, bookYear, false);
+        books.push(bookObject);
+
+        document.dispatchEvent(new Event(RENDER_EVENT));
+        saveData();
+    }
+
+    function generatedId(){
+        return +new Date();
+    }
+
+    function generatedBookObject(id, title, author, year, isCompleted) {
+        return {
+            id,
+            title,
+            author,
+            year,
+            isCompleted,
+        }
+    }
+
+    function postBook(bookObject) {
+        
+    }
+
+})
