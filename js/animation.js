514bee3d2b37e3500fc21454c2fdba5e04c23957
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2023-12-01 08:28:21 +0700

    add animation on bookLayout

--- js/animation.js
+++ js/animation.js
@@ -11,18 +11,15 @@ CloseBtnId.addEventListener("click", () => {
   navId.classList.remove("show");
 });
 
-// ==== Animate on Scroll Initialize  ==== //
 AOS.init();
 
-// ==== GSAP Animations ==== //
-// ==== LOGO  ==== //
 gsap.from(".logo", {
   opacity: 0,
   y: -10,
   delay: 1,
   duration: 0.5,
 });
-// ==== NAV-MENU ==== //
+
 gsap.from(".nav_menu_list .nav_menu_item", {
   opacity: 0,
   y: -10,
@@ -30,38 +27,66 @@ gsap.from(".nav_menu_list .nav_menu_item", {
   duration: 0.5,
   stagger: 0.3,
 });
-// ==== TOGGLE BTN ==== //
+
 gsap.from(".toggle_btn", {
   opacity: 0,
   y: -10,
   delay: 1.4,
   duration: 0.5,
 });
-// ==== MAIN HEADING  ==== //
+
 gsap.from(".main-heading", {
   opacity: 0,
   y: 20,
   delay: 2.4,
   duration: 1,
 });
-// ==== INFO TEXT ==== //
+
 gsap.from(".info-text", {
   opacity: 0,
   y: 20,
   delay: 2.8,
   duration: 1,
 });
-// ==== CTA BUTTONS ==== //
+
 gsap.from(".btn_wrapper", {
   opacity: 0,
   y: 20,
   delay: 2.8,
   duration: 1,
 });
-// ==== TEAM IMAGE ==== //
+
 gsap.from(".team_img_wrapper img", {
   opacity: 0,
   y: 20,
   delay: 3,
+  duration: 1,
+});
+
+gsap.from(".alt-heading", {
+  opacity: 0,
+  y: 20,
+  delay: 3.15,
+  duration: 1,
+});
+
+gsap.from("#searchBook", {
+  opacity: 0,
+  y: 20,
+  delay: 3.35,
+  duration: 1,
+});
+
+gsap.from(".hero-content", {
+  opacity: 0,
+  y: 20,
+  delay: 3.5,
+  duration: 1,
+});
+
+gsap.from(".list-item", {
+  opacity: 0,
+  y: 20,
+  delay: 3.6,
   duration: 1,
 });
